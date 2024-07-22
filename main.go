eba2c5155cc409b6b687013bb7358cb90938d511
Keenan Nicholson <keenan@Keenans-MacBook-Air.local>
2024-07-22 19:29:28 -0230

    some tests

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
-	"strconv"
 	"strings"
 	"syscall"
 
@@ -16,7 +15,7 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main() {
+func runBot() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Error loading .env file")
@@ -25,82 +24,11 @@ func main() {
 	secret := os.Getenv("DISCORD_BOT_TOKEN")
 
 	discord, err := discordgo.New("Bot " + secret)
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	discord.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
-
-		if m.Author.ID == s.State.User.ID {
-			return
-		}
-
-		if strings.Contains(m.Content, "!loot") {
-
-			item := m.Content[6:]
-			url := "https://www.dnd5eapi.co/api/equipment/" + item
-			method := "GET"
-
-			client := &http.Client{}
-			req, err := http.NewRequest(method, url, nil)
-
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			req.Header.Add("Accept", "application/json")
-
-			res, err := client.Do(req)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			defer res.Body.Close()
-
-			body, err := io.ReadAll(res.Body)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-
-			var equipment Equipment
-
-			err = json.Unmarshal(body, &equipment)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-
-			weapon_category, weapon_range, damage_dice, damage_type, weapon_cost := equipment.Weapon.WeaponCategory, equipment.Weapon.WeaponRange, equipment.Weapon.Damage.DamageDice, equipment.Weapon.Damage.DamageType.Name, equipment.Weapon.Cost.Quantity
-			armor_category, armor_class, str_minimum, stealth_disadvantage, weight, armor_cost := equipment.Armor.ArmorCategory, equipment.Armor.ArmorClass.Base, equipment.Armor.StrMinimum, equipment.Armor.StealthDisadvantage, equipment.Armor.Weight, equipment.Armor.Cost.Quantity
-
-			fmt.Println(url)
-			fmt.Println(equipment)
-			fmt.Println(weapon_category, weapon_range, damage_dice, damage_type, weapon_cost)
-			fmt.Println(armor_category, armor_class, str_minimum, stealth_disadvantage, weight, armor_cost)
-
-			if weapon_category == "" {
-				s.ChannelMessageSend(m.ChannelID,
-					"Armor Category: "+armor_category+"\n"+
-						"Armor Class: "+strconv.Itoa(armor_class)+"\n"+
-						"Strength Minimum: "+strconv.Itoa(str_minimum)+"\n"+
-						"Stealth Disadvantage: "+strconv.FormatBool(stealth_disadvantage)+"\n"+
-						"Weight: "+strconv.Itoa(weight)+"\n"+
-						"Cost: "+strconv.Itoa(armor_cost)+equipment.Armor.Cost.Unit)
-
-			} else {
-				s.ChannelMessageSend(m.ChannelID,
-					"Weapon Category: "+weapon_category+"\n"+
-						"Weapon Range: "+weapon_range+"\n"+
-						"Damage Dice: "+damage_dice+"\n"+
-						"Damage Type: "+damage_type+"\n"+
-						"Cost: "+strconv.Itoa(weapon_cost)+equipment.Weapon.Cost.Unit)
-
-			}
-
-		}
-	})
+	discord.AddHandler(messageCreate)
 
 	discord.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
 
@@ -117,4 +45,82 @@ func main() {
 	<-sc
 
 	fmt.Println("Bot is stopping!")
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if strings.Contains(m.Content, "!loot") {
+		item := m.Content[6:]
+		result, err := getLootInfo(item)
+		if err != nil {
+			s.ChannelMessageSend(m.ChannelID, "Error fetching loot information.")
+			return
+		}
+		s.ChannelMessageSend(m.ChannelID, result)
+	}
+}
+
+func getLootInfo(item string) (string, error) {
+	url := "https://www.dnd5eapi.co/api/equipment/" + item
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Add("Accept", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var equipment Equipment
+	err = json.Unmarshal(body, &equipment)
+	if err != nil {
+		return "", err
+	}
+
+	weapon_category, weapon_range, damage_dice, damage_type, weapon_cost := equipment.Weapon.WeaponCategory, equipment.Weapon.WeaponRange, equipment.Weapon.Damage.DamageDice, equipment.Weapon.Damage.DamageType.Name, equipment.Weapon.Cost.Quantity
+	armor_category, armor_class, str_minimum, stealth_disadvantage, weight, armor_cost := equipment.Armor.ArmorCategory, equipment.Armor.ArmorClass.Base, equipment.Armor.StrMinimum, equipment.Armor.StealthDisadvantage, equipment.Armor.Weight, equipment.Armor.Cost.Quantity
+
+	if weapon_category == "" {
+		return fmt.Sprintf("Armor Category: %s\nArmor Class: %d\nStrength Minimum: %d\nStealth Disadvantage: %t\nWeight: %d\nCost: %d%s",
+			armor_category, armor_class, str_minimum, stealth_disadvantage, weight, armor_cost, equipment.Armor.Cost.Unit), nil
+	}
+	return fmt.Sprintf("Weapon Category: %s\nWeapon Range: %s\nDamage Dice: %s\nDamage Type: %s\nCost: %d%s",
+		weapon_category, weapon_range, damage_dice, damage_type, weapon_cost, equipment.Weapon.Cost.Unit), nil
+}
+
+func testGetLootInfo(item string) {
+	result, err := getLootInfo(item)
+	if err != nil {
+		fmt.Println("Error fetching loot information:", err)
+	} else {
+		fmt.Println(result)
+	}
+}
+
+func main() {
+	if len(os.Args) > 1 && os.Args[1] == "test" {
+		if len(os.Args) != 3 {
+			fmt.Println("Usage: go run main.go test <item>")
+			return
+		}
+		item := os.Args[2]
+		testGetLootInfo(item)
+		return
+	}
+
+	runBot()
 }
--- types.go
+++ types.go
@@ -31,24 +31,21 @@ type ArmorClass struct {
 	DexBonus bool `json:"dex_bonus"`
 }
 
-type Weapon struct {
-	WeaponCategory string `json:"weapon_category"`
-	WeaponRange    string `json:"weapon_range"`
-	Damage         Damage `json:"damage"`
-	Cost           Cost   `json:"cost"`
-	Range          Range  `json:"range"`
-}
-
-type Armor struct {
-	ArmorCategory       string     `json:"armor_category"`
-	ArmorClass          ArmorClass `json:"armor_class"`
-	StrMinimum          int        `json:"str_minimum"`
-	StealthDisadvantage bool       `json:"stealth_disadvantage"`
-	Weight              int        `json:"weight"`
-	Cost                Cost       `json:"cost"`
-}
-
 type Equipment struct {
-	Weapon Weapon `json:"weapon"`
-	Armor  Armor  `json:"armor"`
+	Weapon struct {
+		WeaponCategory string `json:"weapon_category"`
+		WeaponRange    string `json:"weapon_range"`
+		Damage         Damage `json:"damage"`
+		Cost           Cost   `json:"cost"`
+		Range          Range  `json:"range"`
+	}
+
+	Armor struct {
+		ArmorCategory       string     `json:"armor_category"`
+		ArmorClass          ArmorClass `json:"armor_class"`
+		StrMinimum          int        `json:"str_minimum"`
+		StealthDisadvantage bool       `json:"stealth_disadvantage"`
+		Weight              int        `json:"weight"`
+		Cost                Cost       `json:"cost"`
+	}
 }
